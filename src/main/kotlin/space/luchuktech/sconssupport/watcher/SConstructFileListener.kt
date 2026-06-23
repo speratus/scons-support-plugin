4757831342847cadcb699d4f1b78425a608bd434
Andrew Luchuk <andrew.luchuk@outlook.com>
2026-06-23 08:11:33 -0400

    Make tool window available when a new SConstruct file is added

--- src/main/kotlin/space/luchuktech/sconssupport/watcher/SConstructFileListener.kt
+++ src/main/kotlin/space/luchuktech/sconssupport/watcher/SConstructFileListener.kt
@@ -2,10 +2,15 @@
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.newvfs.BulkFileListener
+import com.intellij.openapi.vfs.newvfs.events.VFileCreateEvent
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowManager
 import space.luchuktech.sconssupport.settings.SConsProjectSettings
 
 class SConstructFileListener(private val project: Project) : BulkFileListener {
+
+    private lateinit var sconsToolWindow: ToolWindow
 
     override fun after(events: List<VFileEvent>) {
         if (!SConsProjectSettings.getInstance(project).state.autoSync) return
@@ -19,6 +24,36 @@ class SConstructFileListener(private val project: Project) : BulkFileListener {
 
         if (relevant) {
             SConsResyncService.getInstance(project).scheduleResync()
+        } else {
+            return
+        }
+
+        val sconsEvents = events.filter { event ->
+            val path = event.path
+
+            path.endsWith("SConstruct") ||
+                    path.endsWith("SConscript") ||
+                    (path.contains("site_scons") && path.endsWith(".py"))
+        }
+
+        if (sconsEvents.any { event ->
+            event is VFileCreateEvent
+        }) {
+            getToolWindow().isAvailable = true
         }
     }
+
+    private fun getToolWindow(): ToolWindow {
+        if (!::sconsToolWindow.isInitialized) {
+            val sconsWindow = ToolWindowManager.getInstance(project).getToolWindow("SCons")
+            if (sconsWindow != null) {
+                sconsToolWindow = sconsWindow
+            } else {
+                throw IllegalStateException("No SCons tool window available!")
+            }
+        }
+
+        return sconsToolWindow
+    }
+
 }
